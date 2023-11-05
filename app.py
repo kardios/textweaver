d00bdffb439d179825a289d796b5313b1bdb60e5
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2023-11-05 22:30:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ model_id = "mistral-7b-instruct"
 
 # Create a Streamlit interface header
 st.write("**TextWeaver** :scroll: :lower_left_ballpoint_pen: :computer: AI-Powered Writing Experiment by **Sherwood Analytica**")
-input_text = st.text_area("**Enter** the text you would like to process using the prompt in the box and press the **Let\'s Go** button to proceed. :red[Be mindful that as this is an AI-powered app, you may receive unsuitable or incorrect answers. You should take full responsibility over how you use the generated output.]")
+input_text = st.text_area("**Enter** the text you would like to process using the prompt in the box and press the **Let\'s Go** button to proceed.\n:red[You may get unsuitable or incorrect answers and should take full responsibility over how you use the generated output.]")
