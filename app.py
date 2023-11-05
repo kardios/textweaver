5d1b16c6c07fa4a5ffab8ad32994ab946ce6a900
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2023-11-05 22:44:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ model_id = "mistral-7b-instruct"
 st.write("**TextWeaver** :scroll: :lower_left_ballpoint_pen: :computer: AI-Powered Writing Experiment by **Sherwood Analytica**")
 st.write("**:red[May produce unsuitable or incorrect answers. You bear full responsibility over how you use the output.]**")
 
-select_instruction = st.selectbox("Select a prompt:", ('Summarize', 'Main Points', 'Alternative', 'Improvement', 'Customise'))
+choose_instruction = st.selectbox("Choose an instruction", ('Summarize', 'Main Points', 'Alternative', 'Improvement', 'Customise'))
 
 instruction = ''
-if select_instruction  == 'Summarize':
+if choose_instruction  == 'Summarize':
   instruction = "Read the text below and produce an informative and coherent summary. Include the main ideas and key details from the text. Think step by step."
-elif select_instruction  == 'Main Points':
+elif choose_instruction  == 'Main Points':
   instruction = "Read the text below and summarize the main ideas and key details into bullet points. Recognize the overall structure of the text and create bullet points that reflect this structure. The output should be presented in a clear and organized way. Do not start with any titles."
-elif select_instruction  == 'Alternative':
+elif choose_instruction  == 'Alternative':
   instruction = "Read the text below and highlight any missing or incomplete angles."
-elif select_instruction  == 'Improvement':
+elif choose_instruction  == 'Improvement':
   instruction = "Read the text below and suggest critical areas for improvement."
-elif select_instruction  == 'Customise':
+elif choose_instruction  == 'Customise':
   instruction = "Read the text below and propose follow-up actions."
 
-instruction_text = st.text_input("**Refine** your instruction:", instruction)
+instruction_text = st.text_input("**Refine** the instruction", instruction)
 context_text = st.text_area("**Enter** the text you would like to process using the prompt in the box below and press the **Let\'s Go** button to proceed.")
 
 if st.button('Let\'s Go!'):
