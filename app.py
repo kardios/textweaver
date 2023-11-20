c1cfe108c65134375c96829e2ae821e99ba583c5
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2023-11-20 23:57:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,6 @@ import json
 # Retrieve the Perplexity AI Pro API key from the environment variable
 API_KEY = os.environ["PERPLEXITY_AI_PRO_KEY"]
 
-# Define a function to interact with the LLM for conversation
-def chatgpt_conversation(conversation_log):
-  response = openai.ChatCompletion.create(
-    model = "gpt-4-1106-preview",
-    temperature = 0,
-    messages = conversation_log
-  )
-  conversation_log.append({
-    'role': response.choices[0].message.role,
-    'content': response.choices[0].message.content.strip()
-  })
-  return conversation_log
-
 # Create a Streamlit interface header
 st.write("**TextWeaver** :memo: AI-Powered Writing Experiment :memo: **Sherwood Analytica**")
 st.write("**:red[May produce unsuitable or incorrect answers. You bear full responsibility over how you use the output.]**")
@@ -29,21 +16,25 @@ st.write("**:red[May produce unsuitable or incorrect answers. You bear full resp
 temperature = 0
 model_id = "llama-2-70b-chat"
 
-choose_instruction = st.selectbox("**Choose** an instruction", ('Summarize', 'Bias Check', 'Contrarian', 'Alternative', 'Reflection', 'Customise'))
+choose_instruction = st.selectbox("**Choose** an instruction", ('Shorten the text into a summary', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Compare the text with historical events', 'Generate question-answer pairs', 'Customise your own unique prompt'))
 
 instruction = ''
-if choose_instruction  == 'Summarize':
-  instruction = "Read the text below and produce an informative and coherent summary. Include the main ideas and key details from the text. Think step by step."
-elif choose_instruction == 'Bias Check':
-  instruction = "Read the text below and highlight any possible biases."
-elif choose_instruction == 'Contrarian':
-  instruction = "Read the text below and offer contrarian views."
-elif choose_instruction == 'Alternative':
-  instruction = "Read the text below and highlight any missing or incomplete angles."
-elif choose_instruction == 'Reflection':
-  instruction = "Read the text below and draw similiarities and differences to historical events in the last century."
-elif choose_instruction  == 'Customise':
-  instruction = "Read the text below and propose follow-up actions."
+if choose_instruction  == 'Shorten the text into a summary':
+  instruction = "Produce an informative and coherent summary. Include the main ideas and key details from the text."
+elif choose_instruction == 'Identify possible biases in the text':
+  instruction = "Highlight possible biases in the text."
+elif choose_instruction == 'Seek views disagreeing with the text':
+  instruction = "Offer perspectives that disagree with the text."
+elif choose_instruction == 'Find angles missing from the text':
+  instruction = "Offer perspectives that are missing from the text."
+elif choose_instruction == 'Compare the text with historical events':
+  instruction = "Draw similiarities and differences to historical events in the last century."
+elif choose_instruction  == 'Generate question-answer pairs':
+  instruction = "Generate question-answer pairs from the text."
+elif choose_instruction  == 'Customise your own unique prompt':
+  instruction = "Propose follow-up actions."
+
+instruction = "Read the INPUT below. " + instruction + "\n\nINPUT:"
 
 instruction_text = st.text_input("**Refine** the instruction", instruction)
 context_text = st.text_area("**Enter** the text to process using the above instruction")
